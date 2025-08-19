f682879172d1328efde9b26b5f0a33e8aaf1d0f7
Ben Manning <bmanning@me.com>
2025-08-19 11:40:05 -0400

    create hoot model: COMPLETE

--- models/user.js
+++ models/user.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  username: String,
-  hashedPassword: String
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+  }
 });
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     delete returnedObject.hashedPassword;
   }
-})
+});
 
 const User = mongoose.model('User', userSchema);
 
